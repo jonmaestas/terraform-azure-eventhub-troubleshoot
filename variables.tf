c8a6ed6e8ef0ec5c15b0dbc299f77cfa02da9363
Jon Maestas <jonathan.maestas@outlook.com>
2020-07-21 20:27:10 -0500

    Update variables.tf
    added virtual_network_rules default value sample

--- variables.tf
+++ variables.tf
@@ -11,6 +11,12 @@ variable "eventhubs" {
   default = ["test1", "test2"]
 }
 variable "virtual_network_rules" {
+  default = [
+    "/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourceGroups/my-resource-group/providers/Microsoft.Network/virtualNetworks/my_vnet/subnets/subnet1-private-dev",
+    "/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourceGroups/my-resource-group/providers/Microsoft.Network/virtualNetworks/my_vnet/subnets/subnet1-public-dev",
+    "/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourceGroups/my-resource-group/providers/Microsoft.Network/virtualNetworks/my_vnet/subnets/subnet2-private-dev",
+    "/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourceGroups/my-resource-group/providers/Microsoft.Network/virtualNetworks/my_vnet/subnets/subnet2-public-dev"
+  ]
 }
 // variable "virtual_network_rule_1" { }
 // variable "virtual_network_rule_2" { }
